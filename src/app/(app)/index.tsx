ae2a9136ee91265853d7387112108ce9ab789964
wenbin <wenbinlim93@gmail.com>
2024-02-29 14:53:05 +0800

    membership card styling

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,8 +1,7 @@
-import { Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { useAuth } from "@src/providers/auth";
 import { format } from "date-fns";
-
+import { Image } from "expo-image";
 import theme from "@src/theme";
 
 const MembershipCard = () => {
@@ -11,41 +10,46 @@ const MembershipCard = () => {
   const formatDateToString = (date: string) => {
     if (!date) return "-";
 
-    return format(new Date(date), "dd-MM-yyyy");
+    return format(new Date(date), "dd/MM/yyyy");
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.row}>
-        <Text style={styles.rowKey}>Membership No.</Text>
-        <Text style={styles.rowValue}>{user?.username || "-"}</Text>
-      </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.card}>
+        <Image source={require("assets/icon.png")} style={styles.logoImage} />
 
-      <View style={styles.row}>
-        <Text style={styles.rowKey}>First Name</Text>
-        <Text style={styles.rowValue}>{user?.first_name || "-"}</Text>
-      </View>
+        <View style={styles.row}>
+          <Text style={styles.rowKey}>Name</Text>
+          <Text style={styles.rowValue}>
+            {user?.first_name || ""} {user?.last_name || ""}
+          </Text>
+        </View>
 
-      <View style={styles.row}>
-        <Text style={styles.rowKey}>Last Name</Text>
-        <Text style={styles.rowValue}>{user?.last_name || "-"}</Text>
-      </View>
+        <View style={styles.row}>
+          <Text style={styles.rowKey}>Membership No.</Text>
+          <Text style={styles.rowValue}>{user?.username || "-"}</Text>
+        </View>
 
-      <View style={styles.row}>
-        <Text style={styles.rowKey}>Joined Date</Text>
-        <Text style={styles.rowValue}>{formatDateToString(user?.joined)}</Text>
-      </View>
+        <View style={styles.row}>
+          <Text style={styles.rowKey}>Joined Date</Text>
+          <Text style={styles.rowValue}>
+            {formatDateToString(user?.joined)}
+          </Text>
+        </View>
 
-      <View style={styles.row}>
-        <Text style={styles.rowKey}>Membership Expiry Date</Text>
-        <Text style={styles.rowValue}>{formatDateToString(user?.expire)}</Text>
-      </View>
+        <View style={styles.row}>
+          <Text style={styles.rowKey}>Expiry Date</Text>
+          <Text style={styles.rowValue}>
+            {formatDateToString(user?.expire)}
+          </Text>
+        </View>
 
-      <View style={styles.row}>
-        <Text style={styles.rowKey}>Membership Points</Text>
-        <Text style={styles.rowValue}>{JSON.stringify(user?.points)}</Text>
+        <View style={styles.row}>
+          <Text style={styles.rowKey}>Membership Points</Text>
+          <Text style={styles.rowValue}>{JSON.stringify(user?.points)}</Text>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 export default MembershipCard;
@@ -53,15 +57,32 @@ export default MembershipCard;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: theme.padding.containerY,
-    paddingHorizontal: theme.padding.containerX,
-    rowGap: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    width: "90%",
+    backgroundColor: theme.colors.background,
+    alignItems: "center",
+    paddingVertical: 50,
+    rowGap: 12,
+    borderWidth: 1,
+    borderColor: theme.colors.surface,
+    borderRadius: 16,
+  },
+  logoImage: {
+    width: 100,
+    height: 100,
   },
   row: {
-    columnGap: 16,
+    width: "100%",
   },
   rowKey: {
     fontWeight: "bold",
+    color: theme.colors.primary,
+    textAlign: "center",
   },
-  rowValue: {},
+  rowValue: {
+    textAlign: "center",
+  },
 });
